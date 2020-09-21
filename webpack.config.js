585d3600a30e049b80a0e39f374f73cdd59ca566
ajspalma <palma.ashleyjs@gmail.com>
2020-09-21 14:11:23 +0800

    Added Babel (core, loader, preset-env and preset-react (and some experimental stage presets), and React packages (react and react-dom))

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,16 @@ module.exports = {
     }, 
     devServer: {
         contentBase: './dist'
+    },
+    module: {
+        rules: {
+            test: /\.(js||jsx)$/, // look for .js and .jsx files 
+            exclude: /node_modules/, // ignore node_modules
+            use: ['babel-loader'] // transfile using babel-loader, just like middleware??
+        }
+    },
+    // for the extension like js to assume it is a Js file without .js extension
+    resolve: {
+        extensions: ['js', 'jsx']
     }
 }
